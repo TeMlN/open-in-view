f3b1584c5bf72e932687c375614d1f3e8f008e3c
taem1n1 <youman7953@naver.com>
2024-06-19 15:22:43 +0900

    intial commit

---
+++ src/main/java/me/martial/osiv/OsivApplication.java
@@ -0,0 +1,13 @@
+package me.martial.osiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsivApplication.class, args);
+	}
+
+}
